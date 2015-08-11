0babee91f01fd96b1472ae2e86a798dda54217ed
Pallab Mahmud <pmahmud@uoregon.edu>
2015-08-11 14:10:02 -0700

    fixed jquery dependency

--- js/main.js
+++ js/main.js
@@ -31,14 +31,4 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
 });
